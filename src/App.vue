479176e352e589da584ef32bf3ea5724a03784f6
pranjal gupta <pranjalgupta.gh@gmail.com>
2023-01-17 18:50:00 +0530

    pagination applied

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,71 @@
 <template>
   <div id="app">
     <DeskTopHeader />
-    <ProductContainer
-    v-if="filtersData.length"
-    :dataForFilters="filtersData"
-    :dataForProducts="productsData"
-    :dataForSorting="sortingOptions"
-     />
+    <ProductContainer v-if="filtersData.length" :dataForFilters="filtersData" :dataForProducts="productsData"
+      :dataForSorting="sortingOptions" @handleClickThenActive="getValue" @sortData="getSortedData"
+      :currentPagination="currentPagination" />
   </div>
 </template>
 
 <script>
-import DeskTopHeader from './components/BaseAppShellHeader/DeskTopHeader.vue';
-import ProductContainer from './components/ProductContainer/ProductContainer.vue';
+import DeskTopHeader from "./components/BaseAppShellHeader/DeskTopHeader.vue";
+import ProductContainer from "./components/ProductContainer/ProductContainer.vue";
 
 export default {
   components: {
     DeskTopHeader,
-    ProductContainer
+    ProductContainer,
   },
   data() {
     return {
       productsData: [],
       filtersData: [],
-      sortingOptions: []
-    }
+      sortingOptions: [],
+      currentPagination: 1,
+      sortingValue: "",
+    };
   },
   methods: {
     getProductData() {
-      const axios = require("axios")
-      axios.get("https://pim.wforwoman.com/pim/pimresponse.php/?service=category&store=1&url_key=top-wear-kurtas&page=1&count=20&sort_by=&sort_dir=desc&filter=")
-        .then(response => {
-          console.log(response)
-          this.productsData = response.data.result.products;
-          this.filtersData = response.data.result.filters;
-          this.sortingOptions = response.data.result.sort;
-          console.log(this.productsData)
-        })
-    },  
+      const axios = require("axios");
+      const api = `https://pim.wforwoman.com/pim/pimresponse.php/?service=category&store=1&url_key=top-wear-kurtas&page=${this.currentPagination}&count=20&sort_by=${this.sortingValue}&sort_dir=desc&filter=`;
+      axios.get(api).then((response) => {
+        console.log(response);
+        this.productsData = response.data.result.products;
+        this.filtersData = response.data.result.filters;
+        this.sortingOptions = response.data.result.sort;
+      });
+    },
+
+    // Function is used to get current page number
+    getValue(currentPage) {
+      this.currentPagination = currentPage;
+    },
+
+    getSortedData(sortedValue) {
+      console.log("hey", sortedValue);
+      this.sortingValue = sortedValue;
+    },
   },
   mounted() {
     this.getProductData();
-  }
-}
+  },
+  watch: {
+    currentPagination() {
+      this.getProductData();
+    },
+    sortingValue() {
+      if (this.currentPagination !== 1 && this.sortingValue !== "") {
+        this.currentPagination = 1;
+      }
+      this.getProductData();
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  background-color: #FFFFFF;
+  background-color: #ffffff;
 }
 </style>
--- src/components/ProductContainer/ProductContainer.vue
+++ src/components/ProductContainer/ProductContainer.vue
@@ -4,13 +4,8 @@
     <div class="sort-option-container">
       <!-- hide filter button........... -->
       <div class="hide-filter-container">
-        <img
-          v-if="isHideSideFilters"
-          @click="hideSideFilter"
-          class="hide-all-filter"
-          src="@/assets/sideHilterHide.svg"
-          alt=""
-        />
+        <img v-if="isHideSideFilters" @click="hideSideFilter" class="hide-all-filter" src="@/assets/sideHilterHide.svg"
+          alt="" />
         <button class="show-filter-button" v-else @click="showSideFilters">
           Show Filter
         </button>
@@ -18,19 +13,10 @@
       <!-- applied filters chips........... -->
       <div class="applied-chip-container" v-if="appliedFilters.length">
         <div class="filter-chip">
-          <span
-            class="cross-icon-container"
-            v-for="(appliedFilter, index) in appliedFilters"
-            :key="index"
-          >
+          <span class="cross-icon-container" v-for="(appliedFilter, index) in appliedFilters" :key="index">
             <p class="filter-name">{{ appliedFilter }}</p>
             <span>
-              <img
-                @click="removeAppliedFilter(index)"
-                class="cross-icon"
-                src="@/assets/crossIcon.png"
-                alt=""
-              />
+              <img @click="removeAppliedFilter(index)" class="cross-icon" src="@/assets/crossIcon.png" alt="" />
             </span>
           </span>
         </div>
@@ -38,13 +24,10 @@
       <!-- sorting options dropDown................. -->
       <div class="dropdown-container">
         <select v-model="selectedSortingOption" class="drop-down-box">
-          <option class="drop-options" value="" disabled selected>Sort By</option>
-          <option
-            v-for="(option, index) in sortingOptions"
-            :key="index"
-            class="drop-options"
-            :value="option.code"
-          >
+          <option class="drop-options" value="" disabled selected>
+            Sort By
+          </option>
+          <option v-for="(option, index) in sortingOptions" :key="index" class="drop-options" :value="option.code">
             {{ option.label }}
           </option>
         </select>
@@ -61,46 +44,28 @@
           </button>
         </div>
         <div>
-          <div
-            class="filter-container"
-            v-for="(filters, index) in containFiltersData"
-            :key="index"
-          >
+          <div class="filter-container" v-for="(filters, index) in containFiltersData" :key="index">
             <div class="filter-head">
-              <p>{{ filters.filter_lable }}</p>
-              <span
-                v-if="!particularOpenSubFilter.includes(filters.filter_lable)"
-              >
-                <img
-                  @click="showSubFIlters(filters.filter_lable)"
-                  class="plus-icon"
-                  src="@/assets/plusIcon.png"
-                  alt=""
-                />
+              <p @click="showSubFIlters(filters.filter_lable)">
+                {{ filters.filter_lable }}
+              </p>
+              <span v-if="!particularOpenSubFilter.includes(filters.filter_lable)">
+                <img @click="showSubFIlters(filters.filter_lable)" class="plus-icon" src="@/assets/plusIcon.png"
+                  alt="" />
               </span>
               <span v-else>
-                <img
-                  @click="showSubFIlters(filters.filter_lable)"
-                  class="plus-icon"
-                  src="@/assets/minusIcon.png"
-                  alt=""
-                />
+                <img @click="showSubFIlters(filters.filter_lable)" class="plus-icon" src="@/assets/minusIcon.png"
+                  alt="" />
               </span>
             </div>
             <div v-if="particularOpenSubFilter.includes(filters.filter_lable)">
               <ul class="subfilters">
                 <li v-for="(subFilter, index) in filters.options" :key="index">
-                  <input
-                    class="checkbox"
-                    type="checkbox"
-                    v-model="appliedFilters"
-                    :id="subFilter.value"
-                    :value="subFilter.value"
-                  />
+                  <input class="checkbox" type="checkbox" v-model="appliedFilters" :id="subFilter.value"
+                    :value="subFilter.value" />
                   <label :for="subFilter.value" class="lable-subfilter">{{
-                    subFilter.value
+                    filters.filter_lable + "_" + subFilter.value
                   }}</label>
-                  <!-- <span>({{ subFilter.total }})</span> -->
                 </li>
               </ul>
             </div>
@@ -109,28 +74,26 @@
       </div>
 
       <!-- all products container........... -->
-      <div class="product-container">
-        <div class="image-container">
-          <div
-            class="one-product"
-            v-for="(products, index) in containProductData"
-            :key="index"
-          >
-            <div class="product-image-container">
-              <img class="product-image" :src="products.image" alt="" />
+      <div class="product-container" :style="{ width: isHideSideFilters ? '75%' : '100%' }">
+        <div class="one-product-container">
+          <div v-for="(products, index) in containProductData" :key="index" class=" product-image-container">
+            <div class="product-image">
+              <img class="image-kurta" :src="products.image" alt="" />
+
               <div class="heart-image-container">
                 <img class="heart-image" src="@/assets/heartImage.svg" alt="" />
               </div>
-              <div class="title-container">
-                <p>
-                  {{
-                    products.Product_Title_FH === null
-                      ? products.name
-                      : products.Product_Title_FH
-                  }}
-                </p>
-                <p>Rs. {{ products.price }}</p>
-              </div>
+            </div>
+
+            <div class="title-container">
+              <p>
+                {{
+                  products.Product_Title_FH === null
+                    ? products.name
+                    : products.Product_Title_FH
+                }}
+              </p>
+              <p>Rs. {{ products.price }}</p>
             </div>
           </div>
         </div>
@@ -139,13 +102,22 @@
 
     <!-- Pagination section.................................. -->
     <div class="pagination-container">
-
       <div class="total-page-container">
-             
+        <span>Page {{ activeButton }} of 97 </span>
       </div>
-      
-      <div class="counting-container">
 
+      <div class="counting-container">
+        <button v-for="(pageCount, index) in totalPage" :key="index" class="goto-clickable-page" :style="{
+          backgroundColor: activeButton === pageCount ? '#0C0C0C' : '#FFFFFF',
+          color: activeButton === pageCount ? 'white' : '#303030',
+        }" type="button" @click="handleClickThenActive(pageCount)">
+          {{ pageCount }}
+        </button>
+        <span>
+          <button class="next-button" type="button" @click="gotoNextPage">
+            Next
+          </button>
+        </span>
       </div>
     </div>
   </div>
@@ -154,7 +126,7 @@
 <script>
 export default {
   name: "ProductContainer",
-  props: ["dataForFilters", "dataForProducts", "dataForSorting"],
+  props: ["dataForFilters", "dataForProducts", "dataForSorting", "currentPagination"],
   data() {
     return {
       containFiltersData: [],
@@ -164,6 +136,8 @@ export default {
       appliedFilters: [],
       isHideSideFilters: true,
       selectedSortingOption: "",
+      totalPage: [1, 2, 3, 4, 5, 6],
+      activeButton: 1,
     };
   },
   methods: {
@@ -198,32 +172,49 @@ export default {
     },
 
     // FUnction is used to sort products by selected options from dropdown
-    sortBySelectedOption(selectedVal) {
-      if (selectedVal === "price") {
-        this.containProductData.sort(function(product1, product2){
-          return (product1.price - product2.price)
-        });
-      } else if (selectedVal === "discount") {
-        this.containProductData.sort(function(product1, product2){
-           return (product1.discount - product2.discount)
-        });
-      } else if (selectedVal === "product_position") {
-        this.containProductData.sort(function(product1, product2){
-          return (product1.product_position - product2.product_position)
-        });
+    sortBySelectedOption() {
+      this.$emit('sortData', this.selectedSortingOption)
+    },
+
+    // Function is used for pagination active button
+    handleClickThenActive(pageNo) {
+      if (!(this.activeButton === pageNo)) {
+        this.activeButton = pageNo;
+        this.$emit('handleClickThenActive', this.activeButton)
       } else {
-        return;
+        this.activeButton = 1;
       }
     },
+
+    // Function is used to goto page with increment of one in 
+    // current page using next button
+    gotoNextPage() {
+      if (this.activeButton < 97) {
+        this.activeButton += 1;
+        this.$emit('handleClickThenActive', this.activeButton)
+      }
+      return;
+    },
+
+    // Function is used to get data  from parent component
+    getDataFrom() {
+      this.containFiltersData = this.dataForFilters;
+      this.containProductData = this.dataForProducts;
+      this.sortingOptions = this.dataForSorting;
+    }
   },
   mounted() {
-    this.containFiltersData = this.dataForFilters;
-    this.containProductData = this.dataForProducts;
-    this.sortingOptions = this.dataForSorting;
+    this.getDataFrom();
   },
   watch: {
-    selectedSortingOption(newVale) {
-      this.sortBySelectedOption(newVale);
+    selectedSortingOption() {
+      this.sortBySelectedOption();
+    },
+    dataForProducts() {
+      this.getDataFrom();
+    },
+    currentPagination() {
+      this.activeButton = this.currentPagination
     },
   },
 };
@@ -237,26 +228,34 @@ export default {
   align-items: center;
   justify-content: space-between;
 }
+
 .hide-filter-container {
   width: 20%;
-  margin: 2%;
+  margin: 20px;
 }
+
 .applied-chip-container {
   width: 58%;
+  height: auto;
 }
+
 .filter-chip {
   display: flex;
-  column-gap: 2%;
-  height: 40px;
+  flex-wrap: wrap;
+  column-gap: 10px;
+  height: 35px;
 }
+
 .dropdown-container {
   width: 15%;
-  margin: 1%;
+  margin: 10px;
 }
+
 .hide-all-filter {
   width: 44%;
   cursor: pointer;
 }
+
 .show-filter-button {
   padding: 12px;
   font-size: 16px;
@@ -264,24 +263,27 @@ export default {
   cursor: pointer;
   background-color: #ffffff;
 }
+
 .cross-icon-container {
   display: flex;
   width: auto;
   column-gap: 10px;
-  padding: 20px;
+  padding: 0px 10px;
   align-items: center;
-  border-radius: 20px;
+  border-radius: 30px;
   border: 1px solid black;
-  box-sizing: border-box;
 }
+
 .filter-name {
   white-space: pre;
 }
+
 .cross-icon {
   width: 10px;
   height: 10px;
   cursor: pointer;
 }
+
 .drop-down-box {
   width: 100%;
   height: 40px;
@@ -289,6 +291,7 @@ export default {
   font-size: 16px;
   cursor: pointer;
 }
+
 .drop-options {
   cursor: pointer;
   font-size: 16px;
@@ -298,23 +301,28 @@ export default {
 .filter-product-container {
   width: 100%;
   display: flex;
+  column-gap: 20px;
 }
+
 .filter-container {
   border-bottom: 1px solid #707070;
-  padding: 3%;
+  /* padding: 30px; */
 }
+
 .side-filter-container {
-  width: 22%;
+  flex: 0 0 22%;
   height: auto;
   box-sizing: border-box;
-  padding: 2%;
+  padding: 20px;
   background-color: #ffffff;
   font-size: 16px;
 }
+
 li {
   list-style: none;
   color: #303030;
 }
+
 .clear-button {
   padding: 4px;
   font-size: 12px;
@@ -322,24 +330,30 @@ li {
   cursor: pointer;
   background-color: #ffffff;
 }
+
 .subfilters {
   margin: 0px;
   padding: 0px;
 }
+
 .filter-head {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 5px 0px;
 }
+
 .plus-icon {
   height: 20px;
   width: 20px;
+  cursor: pointer;
   align-items: center;
 }
+
 .checkbox {
   height: 18px;
   width: 18px;
-  margin: 4%;
+  margin: 8px;
   cursor: pointer;
   background-color: black;
 }
@@ -349,25 +363,32 @@ li {
   width: 75%;
   box-sizing: border-box;
 }
-.image-container {
-  display: flex;
-  column-gap: 22px;
-  row-gap: 10px;
-  flex-wrap: wrap;
-}
-/* .product-image {
-  width: 250px;
-  height: auto;
-} */
-.product-image-container {
+
+.product-image {
   position: relative;
 }
+
+.product-image-container {
+  width: 24%;
+}
+
+.image-kurta {
+  width: 100%;
+}
+
+.one-product-container {
+  display: flex;
+  row-gap: 20px;
+  column-gap: 12px;
+  flex-wrap: wrap;
+}
+
 .heart-image-container {
   position: absolute;
-  /* position: fixed; */
   top: 10px;
   right: 5px;
 }
+
 .heart-image {
   width: 27px;
 }
@@ -376,6 +397,37 @@ li {
 .pagination-container {
   width: 100%;
   border-top: 1px solid black;
-  margin: 3% 0%;
+  margin: 30px 0px;
+  padding: 30px 0px;
+  display: flex;
+}
+
+.total-page-container {
+  width: 15%;
+}
+
+.counting-container {
+  width: 80%;
+  margin-left: 10px;
+  font-size: 16px;
+  color: #4C0B36;
+  text-align: center;
+}
+
+.goto-clickable-page {
+  padding: 10px;
+  margin: 0px 6px;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+}
+
+.next-button {
+  padding: 10px;
+  color: #4C0B36;
+  font-size: 16px;
+  background-color: #ffffff;
+  border: 1px solid #0c0c0c;
+  cursor: pointer;
 }
 </style>
